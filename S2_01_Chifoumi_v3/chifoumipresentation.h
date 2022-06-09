b80cde7799a724c8bfce9cbd0de2c10d966ed740
NicolasDussin <97294979+NicolasDussin@users.noreply.github.com>
2022-06-09 15:07:11 +0200

    Chifoumi_V3
    V3 du projet de sae2.01 chifoumi

---
+++ S2_01_Chifoumi_v3/chifoumipresentation.h
@@ -0,0 +1,37 @@
+#ifndef CHIFOUMIPRESENTATION_H
+#define CHIFOUMIPRESENTATION_H
+
+#include <QObject>
+#include "chifoumimodele.h"
+
+class ChifoumiVue;
+
+
+class ChifoumiPresentation : public QObject
+{
+    Q_OBJECT
+public:
+    explicit ChifoumiPresentation(ChifoumiModele *m, QObject *parent = nullptr);
+public:
+    ChifoumiModele* getModele();
+    ChifoumiVue* getVue();
+    void setModele(ChifoumiModele *m);
+    void setVue(ChifoumiVue *v);
+
+public slots :
+    void nouvellePartie(); // démarre une nouvelle partie
+    void jouerCoupPierre(); // le joueur joue la pierre
+    void jouerCoupPapier(); // le joueur joue le papier
+    void jouerCoupCiseau(); // le joueur joue le ciseau
+    void APropos(); // demande a afficher la QMessageBox décrivant l'application
+
+private :
+    void jouerTour(); // joue un tour complet
+
+private:
+    ChifoumiModele *_leModele;
+    ChifoumiVue *_laVue;
+
+};
+
+#endif // CHIFOUMIPRESENTATION_H
